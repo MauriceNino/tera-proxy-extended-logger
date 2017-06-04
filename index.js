2251df23bf00f7d96b6df313d2621e403d5bb07c
MauriceNino <mauriceprivat98@gmail.com>
2017-06-05 01:55:35 +0200

    removed desc

--- index.js
+++ index.js
@@ -1,27 +1,5 @@
 const protocol = require('tera-data-parser');
-/*
-Desc: This program is designed to find packages and spy on them for programming purposes
--> First you look for the raw package(s)
--> When you found your package(s), you spy on them with event hooks
 
-Features:
--> Everything controlable through the tera C_CHAT
--> Easy customizable commands, aliases and functions
--> Good errorhandling
-
-How to use:
--> Call "!logger raw" or synonyms like "!l r" to get all the raw packets
--> Call "!logger ignore S_SOCIAL" or synonyms like "!l ign S_SOCIAL" to blacklist those packets from the raw logger
--> Call "!logger data" or synonyms like "!l d" to get all event hooks (empty at the beginning)
--> Call "!logger addDatapackage S_SOCIAL" or synonyms like "!l adp S_SOCIAL" to add a package to the event hooks
--> Call "!logger reset" to reset the current session and start again
-
-Disclaimer:
-You can contact me on discord with the username "MauriceNino#1098" or per email on "mauriceprivat98@gmail.com"
-You can use this project however you want. i dont care
-Ty Pinkie for the raw hook <3
-
-*/
 module.exports = function Logger(dispatch){
   let RAW_HOOK=null,                        //Do not change
       RAW_HOOK_IGNORED_PACKETS=[],          //Predefine packets you want to ignore in the raw hook like ["S_SOCIAL", "C_CHAT"]
